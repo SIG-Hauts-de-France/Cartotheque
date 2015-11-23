4abe5027d827391a6f607dfe5ed81bb22313bc65
Aurélien Bonanni <abonanni@teicee.com>
2015-11-23 14:19:04 +0000

    Mise en place de la home page

--- themes/cartotheque/templates/page.tpl.php
+++ themes/cartotheque/templates/page.tpl.php
@@ -84,10 +84,10 @@
 				print '<li class="user"><span>' . l('', 'user') . '</span>' . t('my account') . "</li>\n";
 				//print ' &nbsp;|&nbsp; ';
 				//print l(t('logout'), 'logout');
-				print '<li class="connect"><span>' . l('', 'logout') . '</span>' . t('logout') . "</li>\n";
+				print '<li class="logout"><span>' . l('', 'user/logout') . '</span>' . t('logout') . "</li>\n";
 			} else {
 				//print l(t('login'), 'user');
-				print '<li class="connect"><span>' . l('', 'user') . '</span>' . t('login') . "</li>\n";
+				print '<li class="login"><span>' . l('', 'user') . '</span>' . t('login') . "</li>\n";
 			}
 		?>
 	</ul>
@@ -114,26 +114,117 @@
 
     <div id="main-wrapper" class="container"><div id="main" class="row clearfix">
 	
-    <?php if ($breadcrumb): ?><div id="breadcrumb" class="col-md-12">
-	 <?php print $breadcrumb; ?>
-	</div><?php endif; ?>
+	<?php if ($breadcrumb): ?>
+		<div id="breadcrumb" class="col-md-12"><?php print $breadcrumb; ?></div>
+	<?php endif; ?>
 	
-	<?php if ($messages): ?><div class="col-md-12">
-    <?php print $messages; ?>
-	</div><?php endif; ?>
-	
-      <div id="mainContent" class="column"><div class="section">
-        <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
+	<?php if ($messages): ?>
+		<div class="col-md-12"><?php print $messages; ?></div>
+	<?php endif; ?>
+
+	<div id="mainContent" class="column">
         <a id="main-content"></a>
-        <?php print render($title_prefix); ?>
-        <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
-        <?php print render($title_suffix); ?>
-        <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
-        <?php print render($page['help']); ?>
-        <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
-        <?php print render($page['content']); ?>
-        <?php print $feed_icons; ?>
-      </div></div> <!-- /.section, /#content -->
+	<?php if (drupal_is_front_page()): ?>
+		<section class="row tvt">
+		<div class="trouver">
+			<div class="lineIcone">
+				<div class="line"></div>
+				<div class="icone loupe"></div>
+			</div>
+			<span class="trouver">trouver</span>
+			</div>
+		<div class="visualiser">
+			<div class="lineIcone">
+				<div class="line"></div>
+				<div class="icone oeil"></div>
+			</div>
+			<span class="visualiser">visualiser</span>
+		</div>
+		<div class="telecharger">
+			<div class="lineIcone">
+				<div class="line"></div>
+				<div class="icone telecharger"></div>
+			</div>
+			<span class="telecharger">télécharger</span>
+		</div>
+		<p>les cartes et leurs métadonnées de la Région<br/><strong><?php if($site_slogan) print $site_slogan; ?></strong></p>
+		</section>
+
+		<!-- Search block -->
+		<section class="row searchMap">
+		<div class="col-sm-6">
+			<form>
+				<div class="input-group">
+				  <span class="input-group-addon search" id="basic-addon1"></span>
+				  <input type="text" class="form-control" placeholder="Recherche" aria-describedby="basic-addon1">
+				</div>
+			</form>
+			<p class="searchMore"><span></span><a href="">Recherche avancée</a></p>
+		</div>
+		</section>
+
+		<!-- Maps block -->
+		<section class="row actus">
+			<div class="col-sm-3">
+			<h2>Dernières cartes ajoutées</h2>
+			<div class="row maps leftMaps">
+				<div class="col-xs-6 col-sm-12 col-md-6"><a href="#"><img src="images/ap-carte-home.jpg" class="img-responsive" alt="" />
+				<span>Titre de la carte</span></a><div class="mapZoom"><img src="images/carte-home.jpg" alt="" /><span>Titre de la carte</span></div></div>
+				<div class="col-xs-6 col-sm-12 col-md-6"><a href="#"><img src="images/ap-carte-home.jpg" class="img-responsive" alt="" />
+				<span>Titre de la carte</span></a><div class="mapZoom"><img src="images/carte-home.jpg" alt="" /><span>Titre de la carte</span></div></div>
+				<div class="col-xs-6 col-sm-12 col-md-6"><a href="#"><img src="images/ap-carte-home.jpg" class="img-responsive" alt="" />
+				<span>Titre de la carte</span></a><div class="mapZoom"><img src="images/carte-home.jpg" alt="" /><span>Titre de la carte</span></div></div>
+				<div class="col-xs-6 col-sm-12 col-md-6"><a href="#"><img src="images/ap-carte-home.jpg" class="img-responsive" alt="" />
+				<span>Titre de la carte</span></a><div class="mapZoom"><img src="images/carte-home.jpg" alt="" /><span>Titre de la carte</span></div></div>
+			</div>
+			</div>
+
+			<div class="col-sm-6">
+				<h2>Cartes du mois</h2>
+				<ul class="rslides">
+					<li><img src="images/carte-home.jpg" alt=""></li>
+					<li><img src="images/carte2.jpg" alt=""></li>
+					<li><img src="images/carte3.jpg" alt=""></li>
+				</ul>
+			</div>
+
+			<div class="col-sm-3">
+				<h2>Cartes les plus téléchargées</h2>
+				<div class="row maps rightMaps">
+					<div class="col-xs-6 col-sm-12 col-md-6">
+						<a href="#"><img src="images/ap-carte-home.jpg" class="img-responsive" alt="" /><span>Titre de la carte</span></a>
+						<div class="mapZoom"><img src="images/carte-home.jpg" alt="" /><span>Titre de la carte</span></div>
+					</div>
+					<div class="col-xs-6 col-sm-12 col-md-6">
+						<a href="#"><img src="images/ap-carte-home.jpg" class="img-responsive" alt="" /><span>Titre de la carte</span></a>
+						<div class="mapZoom"><img src="images/carte-home.jpg" alt="" /><span>Titre de la carte</span></div>
+					</div>
+					<div class="col-xs-6 col-sm-12 col-md-6">
+						<a href="#"><img src="images/ap-carte-home.jpg" class="img-responsive" alt="" /><span>Titre de la carte</span></a>
+						<div class="mapZoom"><img src="images/carte-home.jpg" alt="" /><span>Titre de la carte</span></div>
+					</div>
+					<div class="col-xs-6 col-sm-12 col-md-6">
+						<a href="#"><img src="images/ap-carte-home.jpg" class="img-responsive" alt="" /><span>Titre de la carte</span></a>
+						<div class="mapZoom"><img src="images/carte-home.jpg" alt="" /><span>Titre de la carte</span></div>
+					</div>
+				</div>
+			</div>
+		</section>
+		
+	<?php else: ?>
+		<section>
+		<?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
+		<?php print render($title_prefix); ?>
+		<?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+		<?php print render($title_suffix); ?>
+		<?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+		<?php print render($page['help']); ?>
+		<?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+		<?php print render($page['content']); ?>
+		<?php print $feed_icons; ?>
+		</section>
+	<?php endif; ?>
+      	</div> <!-- /#content -->
 
     </div></div> <!-- /#main, /#main-wrapper -->
 
