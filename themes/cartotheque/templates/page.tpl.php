700d6c35bebb24de6bd14b3e5d1020e0a3f968db
Aurélien Bonanni <abonanni@teicee.com>
2015-11-27 18:17:08 +0000

    Revue affichage liste

--- themes/cartotheque/templates/page.tpl.php
+++ themes/cartotheque/templates/page.tpl.php
@@ -181,19 +181,19 @@
 			<?php print $feed_icons; ?>
 			</div>
 		<?php else: ?>
-			<!--
 			<?php print render($title_prefix); ?>
 			<?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
 			<?php print render($title_suffix); ?>
-			-->
-			<div class="col-sm-8">
+			<div <?php if($page['search']): ?> class="col-sm-8" <?php endif; ?>>
 			<?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
 			<?php print render($page['help']); ?>
 			<?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
 			<?php print render($page['content']); ?>
 			<?php print $feed_icons; ?>
 			</div>
+			<?php if($page['search']): ?>
 			<div class="col-sm-4"><?php print render($page['search']); ?></div>
+			<?php endif; ?>
 		<?php endif; ?>
 		</section>
 	</div>
