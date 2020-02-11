ef1faddbe3fad67cfea3791dcb866445fea4642c
Aurélien Bonanni <abonanni@teicee.com>
2020-02-11 14:16:25 +0000

    ref #3970 - revue mise en page fiche

--- themes/cartotheque/templates/html.tpl.php
+++ themes/cartotheque/templates/html.tpl.php
@@ -60,12 +60,11 @@
   _paq.push(["enableLinkTracking"]);
 
   (function() {
-    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://www.piwik.nordpasdecalais.fr/";
+    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://piwik.hautsdefrance.fr/";
     _paq.push(["setTrackerUrl", u+"piwik.php"]);
     _paq.push(["setSiteId", "9"]);
     var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
-    g.defer=true; g.async=true; g.src=u+"piwik.js";
-	s.parentNode.insertBefore(g,s);
+    g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
   })();
 </script>
 <!-- End Piwik Code -->
